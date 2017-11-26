2b6a5f8b2d75e9b536e600bdfbf10668e7e24750
holubv <holubvojtech@gmail.com>
2017-11-26 21:26:12 +0100

    Include native libraries

--- bootstrap/src/main/java/com/gmail/holubvojtech/snakes/Bootstrap.java
+++ bootstrap/src/main/java/com/gmail/holubvojtech/snakes/Bootstrap.java
@@ -1,8 +1,19 @@
 package com.gmail.holubvojtech.snakes;
 
+import java.io.File;
+
 public class Bootstrap {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+
+        String natives = new File("./natives").getAbsolutePath();
+
+        if (System.getProperty("org.lwjgl.librarypath") == null) {
+            System.setProperty("org.lwjgl.librarypath", natives);
+        }
+        if (System.getProperty("java.library.path") == null) {
+            System.setProperty("java.library.path", natives);
+        }
 
         System.out.println("Hello, world!");
 
